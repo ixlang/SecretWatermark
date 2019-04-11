d137982468febe2811805384b6361a446d32276b
ixlang <dxmer@qq.com>
2019-04-11 10:25:23 +0800

    v1.0

---
+++ fftw.cpp
@@ -0,0 +1,112 @@
+#include <stdint.h>
+#include <xnl.h>
+
+
+XNLEXPORT void * XI_CDECL alloc_complex(xint size){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL free_complex(void * p){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL fill_complex_double(void * p, xint offset, xint size, xint eoffset, xptr rnp){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL fill_complex_double2(void * p, xint offset, xint size, xint eoffset, xptr rnp, xptr inp){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL extract_complex_double(void * p, xint offset, xint size, xint eoffset, xptr rnp){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL extract_complex_double2(void * p, xint offset, xint size, xint eoffset, xptr rnp, xptr inp){
+	
+	
+	
+}
+
+XNLEXPORT double XI_CDECL get_complex_double(void * p, xint index, xbool bi){
+	
+	
+	
+}
+
+XNLEXPORT void * XI_CDECL create_fftw_plan_dft_3d(xint n0, xint n1, xint n2, void * in, void * out, xint sign, xint flag){
+	
+	
+	
+}
+
+XNLEXPORT void * XI_CDECL create_fftw_plan_dft_2d(xint n0, xint n1, void * in, void * out, xint sign, xint flag){
+	
+	
+	
+}
+
+XNLEXPORT void * XI_CDECL create_fftw_plan_dft_1d(xint n, void * in, void * out, xint sign, xint flag){
+	
+	
+	
+}
+
+XNLEXPORT void * XI_CDECL create_fftw_plan_c2r_1d(xint n, void * in, xptr out, xint sign, xint flag){
+	
+	
+	
+}
+
+XNLEXPORT void * XI_CDECL create_fftw_plan_r2c_1d(xint n, xptr in, void * out, xint sign, xint flag){
+	
+	
+	
+}
+
+XNLEXPORT void * XI_CDECL create_fftw_plan_c2r_2d(xint n0, xint n1, void * in, xptr out, xint sign, xint flag){
+	
+	
+	
+}
+
+XNLEXPORT void * XI_CDECL create_fftw_plan_r2c_2d(xint n0, xint n1, xptr in, void * out, xint sign, xint flag){
+	
+	
+	
+}
+
+XNLEXPORT void * XI_CDECL create_fftw_plan_c2r_3d(xint n0, xint n1, xint n2, void * in, xptr out, xint sign, xint flag){
+	
+	
+	
+}
+
+XNLEXPORT void * XI_CDECL create_fftw_plan_r2c_3d(xint n0, xint n1, xint n2, xptr in, void * out, xint sign, xint flag){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL execute_fftw(void * p){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL free_plan(void * p){
+	
+	
+	
+}
+
---
+++ libQXLibrary.cpp
@@ -0,0 +1,448 @@
+#include <stdint.h>
+#include <xnl.h>
+
+
+XNLEXPORT void XI_CDECL widget_set_vint_value(xlong h, xint proid, xint value){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_set_object_value(xlong h, xint proid, XObject * value){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_set_long_object_value(xlong h, xint proid, xlong i, XObject * value){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_set_int2_object_value(xlong h, xint proid, xint i, xint iv, XObject * value){
+	
+	
+	
+}
+
+XNLEXPORT xbool XI_CDECL widget_get_bool_value(xlong h, xint proid){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL widget_get_object(xlong h, xint proid){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL locaUiData(xlong data, XObject * _this, xlong parent){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL locaUiFile(xstring uifile, XObject * _this, xlong parent){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL ShowUi(xlong h){
+	
+	
+	
+}
+
+XNLEXPORT double XI_CDECL widget_get_double_value(xlong h, xint proid){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_set_double_value(xlong h, xint proid, double v){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_set_bool_value(xlong h, xint proid, xbool v){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_set_int_bool_value(xlong h, xint proid, xint v, xbool v1){
+	
+	
+	
+}
+
+XNLEXPORT xbool XI_CDECL widget_get_int_bool(xlong h, xint proid, xint v){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_set_bkrl(xlong h, xint r){
+	
+	
+	
+}
+
+XNLEXPORT xint XI_CDECL widget_get_int_value(xlong h, xint proid){
+	
+	
+	
+}
+
+XNLEXPORT xint XI_CDECL widget_set_v2int_value(xlong h, xint proid, xint xv, xint yv){
+	
+	
+	
+}
+
+XNLEXPORT xint XI_CDECL widget_set_v2int_double_value(xlong h, xint proid, xint xv, double w, xint yv){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_set_native_value(xlong h, xint proid, xlong value){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_slot_string(xlong h, xint proid, xstring value){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_slot(xlong h, xint proid){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL createQxApplication(XObject * obj){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL ApplicationRun(xlong g){
+	
+	
+	
+}
+
+XNLEXPORT xbool XI_CDECL core_sub_class(xlong h, XObject * pthis, XObject * subclass){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL findControl(xlong h, xstring name){
+	
+	
+	
+}
+
+XNLEXPORT xstring XI_CDECL core_getName(xlong h){
+	
+	
+	
+}
+
+XNLEXPORT xstring XI_CDECL core_getClassName(xlong h){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL core_getParent(xlong h){
+	
+	
+	
+}
+
+XNLEXPORT xstring XI_CDECL core_getString(xlong h, xint proid){
+	
+	
+	
+}
+
+XNLEXPORT xstring XI_CDECL core_getStringlongint(xlong h, xint proid, xlong v1, xint v2){
+	
+	
+	
+}
+
+XNLEXPORT xint XI_CDECL core_getintlong(xlong h, xint proid, xlong v){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL createQObject(xint type, XObject * xobj, xlong parent){
+	
+	
+	
+}
+
+XNLEXPORT xint XI_CDECL widget_set_intstring_value(xlong h, xint proid, xint xv, xstring yv){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL createNObject(xint type, xlong param){
+	
+	
+	
+}
+
+XNLEXPORT xint XI_CDECL widget_set_v3int_value(xlong h, xint proid, xint xv, xint yv, xint zv){
+	
+	
+	
+}
+
+XNLEXPORT xint XI_CDECL widget_set_intintstring_value(xlong h, xint proid, xint xv, xint yv, xstring zv){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_set_intlongstring_value(xlong h, xint proid, xlong xv, xint yv, xstring zv){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL widget_set_intlongint_value(xlong h, xint proid, xlong xv, xint yv, xint zv){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL object_get_string(xlong h, xint proid, xstring name){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL object_get_handle_string(xlong h, xint proid, xlong handle, xstring name){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL object_get_string2(xlong h, xint proid, xstring v1, xstring v2){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL object_get_handle(xlong h, xint proid, xlong h1){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL object_get_handle2(xlong h, xint proid, xlong hv, xlong h1){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL object_set_long_int_long(xlong h, xint proid, xlong hv, xint iv, xlong h1){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL object_get_long_int(xlong h, xint proid, xlong hv, xint iv){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL object_get_string_handle_string2(xlong h, xint proid, xstring text, xlong handle, xstring v1, xstring v2){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL object_get_string_handle_string_int(xlong h, xint proid, xstring text, xlong handle, xstring v1, xint v2){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL object_get_string2_handle_string2(xlong h, xint proid, xstring h1, xstring text, xlong handle, xstring v1, xstring v2){
+	
+	
+	
+}
+
+XNLEXPORT XObject * XI_CDECL object_get_string2_handle_string_int(xlong h, xint proid, xstring h1, xstring text, xlong handle, xstring v1, xint v2){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL attachControl(xlong handle, XObject * _this, xstring name){
+	
+	
+	
+}
+
+XNLEXPORT xstring XI_CDECL openfile_dlg_string3_obj(xint proid, xstring caption, xstring defaultPath, xstring pattern, xlong parent){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL long_string2(xlong handle, xint proid, xstring v1, xstring v2){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL long_object_string(xlong handle, xint proid, XObject * v1, xstring v2){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL long_longstring(xlong handle, xint proid, xlong v1, xstring v2){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL long_long_string2(xlong handle, xint proid, xlong l1, xstring v1, xstring v2){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL long_get(xlong handle, xint proid){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL core_attach(xlong h, XObject * x){
+	
+	
+	
+}
+
+XNLEXPORT xint XI_CDECL long_string2_int2(xlong handle, xint proid, xstring v1, xstring v2, xint flags, xint v3){
+	
+	
+	
+}
+
+XNLEXPORT xbool XI_CDECL string_bool5_int2_bool2(xlong handle, xint proid, xstring text, xbool regex, xbool bCase, xbool bWholeWord, xbool warp, xbool forward, xint startLine, xint startIndex, xbool show, xbool posix){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL long_intstring(xlong handle, xint proid, xint type, xstring v1){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL long_intlong(xlong handle, xint proid, xlong value){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL long_intlong2(xlong handle, xint proid, xlong value, xlong v1){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL pointer_intlong2(xptr handle, xint proid, xlong value, xlong v1){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL void_long2(xlong handle, xint proid, xlong value1, xlong v2){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL native_double2(xlong h, xint proid, double v1, double v2){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL native_double4(double v1, double v2, double v3, double v4, xlong h, xint proid){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL native_int4(xlong h, xint proid, xint v1, xint v2, xint v3, xint v4){
+	
+	
+	
+}
+
+XNLEXPORT xint XI_CDECL int_long_int_long_int_int(xlong h, xint proid, xlong h1, xint v1, xint v2){
+	
+	
+	
+}
+
+XNLEXPORT xbool XI_CDECL array_int2(xlong h, xint proid, XObject * $UNUSED_PARAM_2, xint $UNUSED_PARAM_3, xint $UNUSED_PARAM_4){
+	
+	
+	
+}
+
+XNLEXPORT xint XI_CDECL widget_set_bint_value(xlong h, xint proid, xint value){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL create_array_int2(xlong h, xint proid, XObject * obj, xint pos, xint len){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL createSObject(xint type, xstring param){
+	
+	
+	
+}
+
+XNLEXPORT void XI_CDECL long_long_int9(xlong h, xint proid, xlong v1, xint x1, xint x2, xint x3, xint x4, xint y1, xint y2, xint y3, xint y4, xint z1){
+	
+	
+	
+}
+
+XNLEXPORT xlong XI_CDECL long_double2(xlong h, xint proid, double v1, double v2){
+	
+	
+	
+}
+
